e7fe5f087b13c2a29481085d4e5ec8a295ff8361
amit opencart <amitkumar.qa532@webkul.in>
2023-03-13 18:02:46 +0530

    first commit

---
+++ src/Firsttestclass.java
@@ -0,0 +1,37 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Firsttestclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		invoked browser
+		WebDriver driver = new ChromeDriver();
+		
+//		geckoddriver
+//		System.setProperty("webdriver.geckod.driver", "/home/users/amit.opencart/Downloads/geckodriver-v0.32.2-linux64");
+//		WebDriver driver = new FirefoxDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.get("http://localhost/preorder/public/");
+		
+		
+		System.out.println(driver.getTitle());
+		
+		System.out.println(driver.getCurrentUrl());
+		
+//		driver.close();
+		driver.quit();
+	}
+
+}
