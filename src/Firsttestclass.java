19075ad27f87fc5c2d4c0016f6b458186d79236f
amit opencart <amitkumar.qa532@webkul.in>
2023-03-16 18:40:31 +0530

    core java

--- src/Firsttestclass.java
+++ src/Firsttestclass.java
@@ -1,3 +1,4 @@
+import java.sql.Driver;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
@@ -5,6 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Firsttestclass {
@@ -13,24 +15,26 @@ public class Firsttestclass {
 		// TODO Auto-generated method stub
 		
 //		invoked browser
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		 chromeOptions.addArguments("--remote-allow-origins=*");
+		WebDriver Driver = new ChromeDriver(chromeOptions);
 		
 //		geckoddriver
 //		System.setProperty("webdriver.geckod.driver", "/home/users/amit.opencart/Downloads/geckodriver-v0.32.2-linux64");
 //		WebDriver driver = new FirefoxDriver();
 		
-		driver.manage().window().maximize();
+		Driver.manage().window().maximize();
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
-		driver.get("http://localhost/preorder/public/");
+		Driver.get("http://localhost/preorder/public/");
 		
-		System.out.println(driver.getTitle());
+		System.out.println(Driver.getTitle());
 		
-		System.out.println(driver.getCurrentUrl());
+		System.out.println(Driver.getCurrentUrl());
 		
 //		driver.close();
-		driver.quit();
+		Driver.quit();
 	}
 
 }
---
+++ src/corejava.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+
+public class corejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		String website = "This is testing website";
+		char letter = 'A';
+		double dec = 5.99;
+		boolean myCard = true;
+		
+		
+		System.out.println(a);
+		System.out.println(website);
+		System.out.println(letter);
+		System.out.println(dec);
+		System.out.println(myCard);		
+		System.out.println(a+ " this is a integer value.");		
+		System.out.println("This is a example.");
+		System.out.println("This is "+a+" integer value.");
+		
+//		Array		
+		int[] arr = new int [10];
+		arr [0] = 1;
+		arr [1] = 2;
+		arr [2] = 3;
+		arr [3] = 4;
+		arr [4] = 5;
+		
+		int[] arr2 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		
+//		print array one by one
+		System.out.println(arr2[4]);
+		
+//		Print array with loop
+		for (int i = 0; i < arr.length; i++)
+		{
+			System.out.println(i);
+		}
+		
+		for (int i = 0; i < arr2.length; i++)
+		{
+			System.out.println(i);
+		}
+		
+		for (int x: arr)
+		{
+			System.out.println(x);
+		}
+		
+//		array in string
+		
+		String[] name = {"John", "Joe", "Smith"};
+		
+		for (int i = 0; i< name.length; i++)
+		{
+			System.out.println(name[i]);
+		}
+		
+		
+//		run loop with string
+		
+		for (String s: name)
+		{
+			System.out.println(s);
+		}
+		
+//		print value multiple of two in array
+		
+		for (int y = 0; y< arr2.length; y++)
+		{
+			if(arr2[y] % 2 == 0)
+			{
+			System.out.println(arr2[y]+ "is the multiple of 2.");
+			break;
+			}
+			else 
+			{
+				System.out.println(arr2[y]+ "The values are not multiple of 2.");
+			}
+		
+		}
+		
+//		create object of the class - boject.method
+		ArrayList<String> z =  new ArrayList();
+		z.add("Smith");
+		z.add("Goerge");
+		z.add("Joe");
+		z.add("John");
+		z.get(2);
+		System.out.println(z.get(2));
+		
+//		get data into the object array
+		
+		for (int i =0; i< z.size(); i++)
+		{
+			System.out.println(z.get(i));
+		}
+		System.out.println("***********");
+		
+		for (String val :z)
+		{
+			System.out.println(val);
+		}
+		
+//		item is present in Arraylist
+		
+		System.out.println( z.contains("John"));
+		
+	}
+}
