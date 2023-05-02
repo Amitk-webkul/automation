8640f44134d35c43e4f943ebaa496bc1eee9bb0a
amit opencart <amitkumar.qa532@webkul.in>
2023-05-02 16:54:29 +0530

    applying stream, filter and sorting

---
+++ src/filter.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class filter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+		
+		driver.findElement(By.id("search-field")).sendKeys("Rice");
+		
+		List<WebElement> veggies = driver.findElements(By.xpath("//tr/td[1]"));
+		
+//		5 results
+		List<WebElement> filteredList = veggies.stream().filter(veggie-> veggie.getText().contains("Rice")).collect(Collectors.toList());
+		
+		Assert.assertEquals(veggies.size(), filteredList.size());
+		
+		driver.close();
+
+	}
+
+}
